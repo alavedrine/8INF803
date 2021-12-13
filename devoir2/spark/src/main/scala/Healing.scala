fadb828ccbbde161b941c2c0ec1ee13fcb363828
AymLav <aymeric.lavedrine@etu.univ-amu.fr>
2021-12-13 16:44:19 -0500

    Trying to save to json

--- devoir2/spark/src/main/scala/Healing.scala
+++ devoir2/spark/src/main/scala/Healing.scala
@@ -1,8 +1,8 @@
 import Healing.df
 import org.apache.spark
 import org.apache.spark.rdd.PairRDDFunctions
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
-import org.apache.spark.sql.functions.{col, collect_list, explode_outer}
+import org.apache.spark.sql.{DataFrame, Dataset, SaveMode, SparkSession}
+import org.apache.spark.sql.functions.{col, collect_list, collect_set, explode_outer}
 import org.apache.spark.SparkContext._
 
 import scala.collection.mutable
@@ -17,16 +17,18 @@ object Healing extends App {
     .master("local[*]")
     .getOrCreate()
 
-    val savePath = "src/main/reversed_creatures"
+    val savePath = "src/main/reversed_creatures/"
 
     import spark.implicits._
     val df = spark.read.option("multiline", "true").json("src/main/creatures.json").as[Creature]
     val reversedDF = df.select($"name",explode_outer($"spells").as("spell"))
 
     println("DataFrame version :")
-    dataframeVersion(reversedDF)
+    val merged = dataframeVersion(reversedDF)
+    merged.write.format("json").save("src/main/reversed_creatures/")
 
-    def dataframeVersion(df: DataFrame): Unit = {
+
+    def dataframeVersion(df: DataFrame): Dataset[Spell] = {
         val mergedReversedDf = df.groupBy($"spell").agg(collect_list($"name").as("creatures")).as[Spell]
 
         mergedReversedDf.foreach(row => {
@@ -34,7 +36,12 @@ object Healing extends App {
             row.creatures.foreach(creature => print(creature + "; "))
             println()
         })
-
+        return mergedReversedDf
+//        mergedReversedDf.write.json(savePath)
+//        mergedReversedDf
+//          .write
+//          .format("json")
+//          .save(savePath)
 //        mergedReversedDf.rdd.repartition(1).saveAsTextFile(savePath)
     }
 
